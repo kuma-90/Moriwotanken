c273608b3a18f9e85950d03e8314c483e2d00d61
kuma-90 <maharo_keina@icloud.com>
2022-03-01 02:14:22 +0900

    歩くスクリプト追加、動くのはまだ、アニメーターの設定

--- Assets/Script/GameModel.cs
+++ Assets/Script/GameModel.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GameModel : MonoBehaviour
 {
+    int speed = 3;
+
+    public GameObject kumachan;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
     }
 }
